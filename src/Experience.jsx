c0427ef89b45df9da212df973bdb3f954ef3d7f2
metamong-Hi <seojinlee27@naver.com>
2024-04-25 14:39:04 +0900

    캐릭터 움직임에 따라 광원 조절

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,24 +1,48 @@
 import {Environment, OrbitControls, SoftShadows} from "@react-three/drei"
 import { MathUtils } from "three"
 import * as THREE from "three"
-import{Character} from "./Character"
+import Character  from "./Character"
 import {useThree} from "@react-three/fiber"
 import { Physics, RigidBody } from'@react-three/rapier'
 import {Perf} from "r3f-perf"
 import React, { useEffect, useRef } from 'react'
+
+import { useFrame } from "@react-three/fiber"
+
+function FollowShadowLight({refLight,refCharacterRigid}){
+    useFrame(()=>{
+        
+    });
+}
+
+
 export default function Experience(){
     const {gl,scene}=useThree(({gl,scene})=>({gl,scene}));
     useEffect(()=>{gl.toneMappingExposure=0.7},[gl,scene]);
     const refOrbitControls=useRef();
+    const refLight=useRef();
+    const refShadowCameraHelper=useRef();
+    const refCharacterRigid=useRef();
+    const refRigid=useRef();
+    useEffect(()=>{
+        refShadowCameraHelper.current=new THREE.CameraHelper(refLight.current.shadow.camera);
+        scene.add(refShadowCameraHelper.current);
+        scene.add(refLight.current.target);
+        return ()=>{
+            scene.remove(refShadowCameraHelper.current);
+            scene.remove(refLight.current.target);
+        }
+    },[refLight.current]);
+
     return <>
     <Perf position="bottom-left"></Perf>
     <OrbitControls ref={refOrbitControls}/>
     {/* <ambientLight intensity={0.2}/> */}
-    <directionalLight castShadow position={[0,1,2]} intensity={1}/>
+    <directionalLight castShadow ref={refLight} position={[0,1,2]} intensity={1}/>
     <Environment preset="city" background/>
     <SoftShadows size={2} focus={0} samples={8}/>
     <Physics debug>
-    <Character refOrbitControls={refOrbitControls}/>
+    <Character ref={refRigid} refOrbitControls={refOrbitControls}/>
     <RigidBody type="fixed">
 <mesh receiveShadow>  
     <boxGeometry args={[100,0.1,100]}/>
@@ -33,5 +57,6 @@ export default function Experience(){
 </mesh>
 </RigidBody>
 </Physics>
+<FollowShadowLight refLight={refLight} ref={refCharacterRigid}/>
     </>
 }
