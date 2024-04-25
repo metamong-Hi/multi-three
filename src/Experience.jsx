33aae94da1d6e1f1da9c01e0ab9b2aaf63ddeb8a
metamong-Hi <seojinlee27@naver.com>
2024-04-25 11:10:10 +0900

    캐릭터 설정 및 애니메이션 테스트

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,17 @@
-import {OrbitControls} from "@react-three/drei"
+import {Environment, OrbitControls} from "@react-three/drei"
 import { MathUtils } from "three"
 import * as THREE from "three"
+import{Character} from "./Character"
 export default function Experience(){
     return <>
     <OrbitControls/>
     <ambientLight intensity={0.2}/>
-    <directionalLight position={[0,1,2]} intensity={1}/>
+    <directionalLight position={[0,1,2]} intensity={0.1}/>
+    <Environment preset="city" background/>
+    <Character/>
 <mesh  >  
     <boxGeometry args={[100,0.1,100]}/>
-    <meshBasicMaterial/>
+    <meshBasicMaterial color="#5d6d72"/>
 
 </mesh>
     </>
